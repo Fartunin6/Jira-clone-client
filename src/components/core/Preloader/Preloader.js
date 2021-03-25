30d6f7ee791e6834c35409a0997f332b6ef01e35
Fartunin6 <53219903+Fartunin6@users.noreply.github.com>
2021-03-25 13:58:53 +0300

    added preloader

--- src/components/core/Preloader/Preloader.js
+++ src/components/core/Preloader/Preloader.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Preloader = () => {
-    return <div className="preloader-wrapper">
-        <p>Loading...</p>
+  return (
+    <div className="preloader-wrapper">
+      <div className="loader">Loading...</div>
     </div>
+  );
 };
 
 export default Preloader;
