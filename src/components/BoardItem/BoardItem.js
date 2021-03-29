54503485e10aeee00249eac9ff7dea1276b9f31a
Fartunin6 <53219903+Fartunin6@users.noreply.github.com>
2021-03-29 22:26:53 +0300

    fixed unnecessary requests

--- src/components/BoardItem/BoardItem.js
+++ src/components/BoardItem/BoardItem.js
@@ -4,24 +4,26 @@ import yellowStarIcon from '../../assets/images/yellow-star.png';
 import penIcon from '../../assets/images/pen.svg';
 
 const BoardItem = ({ title, background, deleteBoard, saveBoard, changeTitle, _id, isSaved }) => {
-  const [isEditTitle, setIsEditTitle] = useState(false);
+  const [visibleInput, setVisibleInput] = useState(false);
   const [editedTitle, setEditedTitle] = useState(title);
 
   return (
     <div className="board-item" style={{ background: background }}>
       <div className="board-item__title">
-        {isEditTitle ? (
+        {visibleInput ? (
           <>
             <input
               type="text"
               onChange={(e) => setEditedTitle(e.target.value)}
               value={editedTitle}
               className="board-item__input"
-            />{' '}
+            />
             <div
               onClick={() => {
-                changeTitle(_id, editedTitle);
-                setIsEditTitle(false);
+                setVisibleInput(false);
+                if (editedTitle !== title) {
+                  changeTitle(_id, editedTitle);
+                }
               }}
               className="board-item__active"></div>
           </>
@@ -29,7 +31,7 @@ const BoardItem = ({ title, background, deleteBoard, saveBoard, changeTitle, _id
           <>
             <span>{title}</span>
             <img
-              onClick={() => setIsEditTitle(true)}
+              onClick={() => setVisibleInput(true)}
               src={penIcon}
               alt="pen"
               className="board-item__change"
